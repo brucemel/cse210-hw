1370fcdebf3d899629396f800b357e7445b1eff5
brucemel <mel24004@byui.edu>
2024-09-24 17:56:23 -0500

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101); // Generate a number between 1 and 100
+
+        int guess = 0; // Initialize guess
+
+        // Loop until the user guesses the correct number
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+
+            // Validate user input
+            if (int.TryParse(Console.ReadLine(), out guess))
+            {
+                if (guess == number)
+                {
+                    Console.WriteLine("You did it!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, try again!");
+                    if (guess >= number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
     }
 }
