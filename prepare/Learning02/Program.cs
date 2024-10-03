61092101f80629cadb8bbbfef7e1b21395947109
brucemel <mel24004@byui.edu>
2024-10-03 18:11:46 -0500

    02learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        // Create a resume instance
+        Resume myResume = new Resume("Allison Rose");
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    private string _name;
+    private List<Job> _jobs;
+
+    // Constructor
+    public Resume(string name)
+    {
+        _name = name;
+        _jobs = new List<Job>();
+    }
+
+    // Method to add a job
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Method to display resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
